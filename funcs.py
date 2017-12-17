2578b79e9cafefd2d5676b2a3476ff4060d7bec3
Vesperon51 <vesperon51@gmail.com>
2017-12-17 17:29:16 +0100

    Added selector mode and modes. Added screenClear, improved the code and removed the bugs

--- funcs.py
+++ funcs.py
@@ -2,6 +2,7 @@ import random
 import glob, os
 import time
 from pathlib import Path
+import platform
 """Mostra el menu principal i recull l'opcio de l'usuari
 @return option. Opcio desitjada
 @since 5/12/2017
@@ -14,36 +15,67 @@ def menu():
     print("3. Establir dificultat")
     print("4. Carregar fitxer de configuracio")
     print("5. Guardar configuracio actual")
-    print("6. Guardar estadistiques i sortir")
+    print("6. Cambiar mode de joc")
+    print("7. Guardar estadistiques i sortir")
     option = input("Selecciona l'opcio: ")
     return option
+"""Mostra el sub menu del mode de joc i recull l'opcio de l'usuari
+@return option. Opcio desitjada
+@since 17/12/2017
+@version 1.0
+@author Vesperon51"""
+def subMenu():
+    print("Selecciona el nou mode de joc")
+    print("1. Activar/desactivar repeticions de color")
+    print("2. Activar/desactivar lletres")
+    print("3. Activar/desactivar numeros")
+    print("4. Tornar al menu principal")
+    subOption= input("Selecciona l'opcio: ")
+    return subOption
 
 
 """Determina si un numero esta a una llista
 Si el numero generat es repetit torna a generar un numero aleatori i "destrueix" el numero repetit
 @param j. La quantitat de numeros que vols a la llista
+@param maxRange. El rang maxim de generacio d'aleatoris
 @return randNum. Llista amb numeros aleatoris diferents
 @since 10/12/2017
-@version 1.0
+@version 1.1
 @author Vesperon51"""
-def generarCodi(j):
+def generarCodi(j, maxRange):
     i=0
     randNum=[]
     while i<int (j):
-        num=generarRandom()
+        num=generarRandom(maxRange)
         if not num in randNum:
             randNum.append(num)
             i=i+1
+    return randNum
+"""Introdueix numeros aleatoris a una llista independentment si estan repetits
+@param j. La quantitat de numeros que vols a la llista
+@param maxRange. El rang maxim de generacio d'aleatoris
+@return randNum. Llista amb numeros aleatoris
+@since 17/12/2017
+@version 1.1
+@author Vesperon51"""
+def generarCodiRepeticions(j, maxRange):
+    i=0
+    randNum=[]
+    while i<int (j):
+        num=generarRandom(maxRange)
+        randNum.append(num)
+        i=i+1
     return randNum
     
 
 """Genera un numero aleatori
 @num. El numero aleatori generat
+@param maxRange. El rang maxim de generacio d'aleatoris
 @since 10/12/2017
 @version 1.0
 @author Vesperon51"""
-def generarRandom ():
-    num=random.randint(1,15)
+def generarRandom (maxRange):
+    num=random.randint(1,maxRange)
     return num
 
 
@@ -86,6 +118,69 @@ def desxifrarCodi(randNum):
             randCode.append("cobre")
         elif w==15:
             randCode.append("coral")
+    return randCode
+
+"""Asigna un numero a una lletra
+@param randNum. Llista amb numeros
+@return randCode. Llista de colors
+@since 17/12/2017
+@version 1.0
+@author Vesperon51"""
+def desxifrarCodiLletres(randNum):
+    randCode=[]
+    for w in randNum:
+        if w==1:
+            randCode.append("a")
+        elif w==2:
+            randCode.append("b")
+        elif w==3:
+            randCode.append("c")
+        elif w==4:
+            randCode.append("d")
+        elif w==5:
+            randCode.append("e")
+        elif w==6:
+            randCode.append("f")
+        elif w==7:
+            randCode.append("g")
+        elif w==8:
+            randCode.append("h")
+        elif w==9:
+            randCode.append("i")
+        elif w==10:
+            randCode.append("j")
+        elif w==11:
+            randCode.append("k")
+        elif w==12:
+            randCode.append("l")
+        elif w==13:
+            randCode.append("m")
+        elif w==14:
+            randCode.append("n")
+        elif w==16:
+            randCode.append("o")
+        elif w==17:
+            randCode.append("p")
+        elif w==18:
+            randCode.append("q")
+        elif w==19:
+            randCode.append("r")
+        elif w==20:
+            randCode.append("s")
+        elif w==21:
+            randCode.append("t")
+        elif w==22:
+            randCode.append("u")
+        elif w==23:
+            randCode.append("v")
+        elif w==24:
+            randCode.append("w")
+        elif w==25:
+            randCode.append("x")
+        elif w==26:
+            randCode.append("y")
+        elif w==27:
+            randCode.append("z")
     return randCode
 
 
@@ -138,7 +233,7 @@ def comptaCoincidencies(userCode, IACode):
 """Comprova que el codi de l'usuari sigui correcte
 @param userCode. El codi de l'usuari
 @param IACode. El codi de la IA
-@return True si el codi es correcte o fals si no ho esa
+@return True si el codi es correcte o fals si no ho es
 @since 10/12/2017
 @version 1.0
 @author Vesperon51"""
@@ -155,11 +250,22 @@ def esFormatCorrecte(userCode, IACode):
 """Controla la partida fent les invocacions pertinents
 es surt de la partida si les dues llistes son iguals o si s'esgoten  els intents
 @param settings. Llista amb la configuracio de la partida(mida del codi[0], intents disponibles[1])
+@param repeatMode. Boolea que determina si el mode repeticio esta activat
+@param lettersMode. Boolea que determina si el mode lletres esta activat
+@param numMode. Boolea que determina si el mode numero esta activat
 @since 11/12/2017
-@version 1.0
+@version 1.4
 @author Vesperon51"""
-def partida(settings):
-    IACode=desxifrarCodi(generarCodi(settings[0]))
+def partida(settings, repeatMode, lettersMode, numMode):
+    if lettersMode:
+        IACode=desxifrarCodiLletres(generarCodi(settings[0], 24))
+    else:
+        if repeatMode:
+            IACode=desxifrarCodi(generarCodiRepeticions(settings[0], 15))
+        elif numMode:
+            IACode=generarCodi(settings[0], 8)
+        else:
+            IACode=desxifrarCodi(generarCodi(settings[0], 15))
     intents=0
     victoria=True
     while victoria and intents<int(settings[1]):
@@ -187,25 +293,32 @@ def partida(settings):
 El codi ha de ser superior a 2 o menor igual a 8
 Els intents han de ser superior o igual a 2
 L'unica forma de sortir del loop es introduir be les dades
+@param lettersMode. Boolea que determina si el mode lletres esta activat
 @return settings. Llista amb la configuracio de la partida(mida del codi[0], intents disponibles[1])
 @since 11/12/2017
-@version 1.0
+@version 1.2
 @author Vesperon51"""
-def definirDif():
+def definirDif(lettersMode):
     settings=[]
     flag1=False
     flag2=False
+    oport=0
+    maxLength=8
+    if lettersMode==True:               #Si el mode de lletres esta activat vol dir que el num maxim del codi es 24
+        maxLength=24
     while True:
-        mida=input("Escriu la mida del codi: ")
-        if mida<="8" and mida>="2":
+        print("Escriu la mida del codi minim:2 maxim:", maxLength)
+        mida=input("")
+        if int(mida)<=maxLength and int (mida)>=2:
             flag1=True
+            oport=input("Escriu les oportunitats minim:2 : ")
+            if int (oport)>=2:
+                flag2=True
+            else:
+                print("Els intents han de ser 2 o mes de 2")
         else:
-            print("La mida a de ser superior a 2 o menor-igual a 8")
-        oport=input("Escriu les oportunitats: ")
-        if oport>="2":
-            flag2=True
-        else:
-            print("Els intents han de ser 2 o mes de 2")
+            print("La mida a de ser superior a 2 o menor-igual a",maxLength)
+        
         if flag1 and flag2:
             settings.append(mida)
             settings.append(oport)
@@ -319,4 +432,12 @@ def generarConfig(settings, cfgFile):
         myFile.write(cadena)
         myFile.close
         return True
-
+"""Determina quin sistema operatiu s'esta executant i llença l'ordre de netejar pantalla adient
+@since 17/12/2017
+@version 1.0
+@author Vesperon51"""
+def clearScreen():
+    if platform.system()=="Windows":
+        os.system('cls')
+    if platform.system()=="Linux":
+        os.system("clear")
--- main.py
+++ main.py
@@ -10,31 +10,46 @@ else:                       #Si no s'inicien les variables a 0 per evitar errors
     partLost=0
     partTot=0
 option=0
+subMenuController=True
+#Booleans que controlen quin mode es juga
+repeatMode=False
+lettersMode=False
+numMode=False
+subOption=0                 #Variable que controla el submenu de mode de joc
 settings=[]
-while option != "6":
+while option != "7":
+    if repeatMode:
+        print("Mode de repeticio activat")
+    if lettersMode:
+        print("Mode de lletres activat")
+    if numMode:
+        print("Mode de numeros activat")
+    if repeatMode==False and numMode==False and lettersMode==False:
+        print("Mode normal activat")
     option=menu()
+    subMenuController=True
     if option == "1":
         print("Nou joc")
         if not settings:                            #Si intenta jugar sense establir una dificultat
             print("Estableix abans dificultat")
         else:
-            if partida(settings):
+            clearScreen()
+            if partida(settings, repeatMode, lettersMode, numMode):
                 partWin=int(partWin)+1
                 partTot=int(partTot)+1                   #Si es determina la variable partTot com a resultat de victories+derrotes l'escripura al fitxer falla, coses del Python
             else:
                 partLost=int(partLost)+1
                 partTot=int(partTot)+1
     elif option == "2":
-        print("Visualitza i guarda les estadistiques")
         estadistica(partWin, partLost, partTot)     #Mostra les stats i les salva al fitxer stats.txt al mateix directori
     elif option == "3":
-        print("Dificultat")
-        settings=definirDif()                       #Obligatori definir dificultat abans de jugar
-    elif option == "6":                             
+        clearScreen()
+        settings=definirDif(lettersMode)                       #Obligatori definir dificultat abans de jugar
+    elif option == "7":                             
         saveStats(partWin, partLost, partTot)
         print("Adeu!")
     elif option == "4":
-        print("Carrega de configuracio")
+        clearScreen()
         listDir()
         cfgFile=input("Selecciona el fitxer que vols carregar: ")
         cfg=loadSettings(cfgFile)                   #Emmagatzemo a la llista cfg la config o el 0
@@ -42,12 +57,53 @@ while option != "6":
             settings=cfg                            #Igualo la llista settings a cfg per poder carregar la configuracio a la partida i poder iniciar el joc
     elif option=="5":
         if not settings:
+            clearScreen()
             print("No hi ha cap configuracio definida")
         else:
-            configName=input("Escriu el nom de la teva configuracio: ")
-            if generarConfig(settings, configName):
-                print("La configuracio s'ha guardat correctament")
+            clearScreen()
+            if lettersMode:                         #Si s'esta amb un mode extra no es pot guardar la config per evitar errors
+                print("No pots guardar la configuracio si estas en un mode extra")
             else:
-                print("Error en guardar la configuracio. El fitxer ja existeix")
+                configName=input("Escriu el nom de la teva configuracio: ")
+                if generarConfig(settings, configName):
+                    print("La configuracio s'ha guardat correctament")
+                else:
+                    print("Error en guardar la configuracio. El fitxer ja existeix")
+    elif option=="6":
+        clearScreen()
+        while subOption!="4" and subMenuController:     #Nomes es pot tenir un mode extra activat a la vegada
+            subOption=subMenu()
+            settings=[]                                 #Reinicia les settings per evitar errors
+            if subOption=="1":                          #Repeat mode
+                if repeatMode:
+                    repeatMode=False
+                    lettersMode=False
+                else:
+                    repeatMode=True
+                    lettersMode=False
+                subMenuController=False
+            elif subOption=="2":                        #Letters mode
+                if lettersMode:
+                    lettersMode=False
+                    repeatMode=False
+                else:
+                    repeatMode=False
+                    lettersMode=True
+                subMenuController=False
+            elif subOption=="3":                        #Numb mode
+                if numMode:
+                    numMode=False
+                    repeatMode=False
+                    lettersMode=False
+                else:
+                    numMode=True
+                    repeatMode=False
+                    lettersMode=False
+                subMenuController=False
+            elif subOption=="4":
+                print("Tornant al menu principal")
+                subMenuController=False
+            else:
+                print("Selecciona una opcio correcte")
     else:
         print("Escriu una opcio adequada")
